06f921372428343feb98c1a48ff849bc81524434
crimama <crimama2@gmail.com>
2024-04-08 14:05:38 +0900

    softpatch added

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,7 @@ from .stpm.stpm import STPM
 from .fastflow.model import FastFlow 
 from .reversedistillation.reversedistillation import ReverseDistillation
 from .softpatch.softpatch import PatchCore
+from .softpatch.softpatch import PatchCore as SoftPatch
 from reconpatch.model import ReconPatch
 from proxycore_base.model import ProxyCoreBase
 from proxycore_base.co_teaching import CoTeaching
